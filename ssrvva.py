782d113150a41eda2bc9f2f6d13a97394d3d9234
Walidname113 <100084394+Walidname113@users.noreply.github.com>
2023-07-12 23:14:00 +0300

    Add files via upload

---
+++ ssrvva.py
@@ -0,0 +1,55 @@
+from .. import loader
+
+
+@loader.tds
+class sssrv(loader.Module):
+    """Отправляет различные видео/гс которые указаны в командах.\nАвтор: @LolDec."""
+
+    strings = {"name": "sssrv"}
+
+    async def ssrvacmd(self, message):
+        """| Отправляет гс с ссср-эдит звуком"""
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/SSSSddjjvvb/5",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def ssrvbcmd(self, message):
+        """| Отправляет гс с БАС-БУСТ ссср-эдит звуком."""
+        reply = await message.get_reply_message()
+        await message.delete()
+        audio_link = "https://t.me/SSSSddjjvvb/13"
+        await message.client.send_file(
+            message.to_id,
+            audio_link,
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def ssrvcmd(self, message):
+        """| Отправляет видео-сообщение с эдитом про политиков в виде скелета"""
+        reply = await message.get_reply_message()
+        await message.delete()
+        video_link = "https://t.me/SSSSddjjvvb/15"
+        await message.client.send_file(
+            message.to_id,
+            video_link,
+            video_note=True,
+            reply_to=reply.id if reply else None,
+        )
+
+    async def ssrvdcmd(self, message):
+        """| Отправляет видео-сообщение с эдитом про немецких войск WW2"""
+        reply = await message.get_reply_message()
+        await message.delete()
+        video_link = "https://t.me/SSSSddjjvvb/16"
+        await message.client.send_file(
+            message.to_id,
+            video_link,
+            video_note=True,
+            reply_to=reply.id if reply else None,
+        )
