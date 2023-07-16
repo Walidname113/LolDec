5d53a61eedc9caf9316e86dcf06c8e8c463f17ee
Walidname113 <100084394+Walidname113@users.noreply.github.com>
2023-07-16 04:42:15 +0300

    Add files via upload

---
+++ Mozaika.py
@@ -0,0 +1,88 @@
+# meta developer: @LolDecMods
+
+import io
+import os
+import zipfile
+from typing import List
+from hikka.types import Message
+
+from PIL import Image, UnidentifiedImageError
+
+from .. import loader, utils
+
+
+@loader.tds
+class ModuleName(loader.Module):
+    """Mozaika разделит ваше фото на несколько ровных частей"""
+
+    strings = {"name": "mozaika"}
+    strings_ru = {"name": "mozaika"}
+
+    @loader.command(alias="mz", ru_doc="| Ответьте реплаем на фото, которое хотите разделить. Пример: <code>.mz <на сколько частей разделить (2-4)> [img/file]</code>")
+    async def mzcmd(self, m: Message):
+        """| Ответьте реплаем на фото, которое хотите разделить. Пример: <code>.mz <на сколько частей разделить (2-4)> [img/file]</code>"""
+        args = utils.get_args_raw(m)
+        split_count, output_format = self._parse_arguments(args)
+        if not split_count:
+            await utils.answer(m, "После команды, укажите сколько частей делать. (От 2 до 4)")
+            return
+        if not output_format:
+            await utils.answer(m, "Пожалуйста, укажите формат вывода: [img/file]")
+            return
+        if split_count < 2 or split_count > 4:
+            await utils.answer(m, "Нельзя сделать больше 4 частей, и меньше 2-ух частей!")
+            return
+        reply_message = await m.get_reply_message()
+        if reply_message and reply_message.media:
+            file = await reply_message.download_media()
+            try:
+                parts = await self._split_file(file, split_count)
+                if output_format == "img":
+                    output_files = [io.BytesIO(part) for part in parts]
+                    await utils.answer_file(m, output_files)
+                elif output_format == "file":
+                    zip_file = await self._create_zip_file(parts)
+                    zip_file.name = "фото.zip"
+                    await utils.answer_file(m, zip_file, force_document=True, caption="Разделенные изображения без потери качества.\n\n"
+                                                                                       "Распакуйте архив, фото в виде файла нельзя скинуть по причинам того, что Telegram не поддерживает такое.")
+            except UnidentifiedImageError:
+                await utils.answer(m, "Тип изображения не поддерживается PIL, это не ошибка, это неисправимо по причинам самой библиотеки.")
+            except ValueError:
+                await utils.answer(m, "Изображение слишком маленькое для разделения.")
+        await m.delete()
+
+    def _parse_arguments(self, args: str) -> tuple[int, str]:
+        split_count = 0
+        output_format = ""
+        if args:
+            args_list = args.split()
+            if args_list[0].isdigit():
+                split_count = int(args_list[0])
+                if len(args_list) > 1:
+                    output_format = args_list[1].lower()
+        return split_count, output_format
+
+    async def _split_file(self, file: str, split_count: int) -> List[bytes]:
+        parts = []
+        with Image.open(os.path.abspath(file)) as img:
+            img_width, img_height = img.size
+            part_width = img_width // split_count
+            if part_width <= 0:
+                raise ValueError("Изображение слишком маленькое для разделения.")
+            for i in range(split_count):
+                left = i * part_width
+                right = (i + 1) * part_width if i < split_count - 1 else img_width
+                part_img = img.crop((left, 0, right, img_height))
+                part_bytes = io.BytesIO()
+                part_img.save(part_bytes, format="PNG")
+                part_bytes.seek(0)
+                parts.append(part_bytes.getvalue())
+        return parts
+
+    async def _create_zip_file(self, parts: List[bytes]) -> io.BytesIO:
+        zip_file = io.BytesIO()
+        with zipfile.ZipFile(zip_file, "w", zipfile.ZIP_DEFLATED) as zf:
+            for i, part in enumerate(parts):
+                zf.writestr(f"Part_{i+1}.png", part)
+        zip_file.seek(0)
+        return zip_file
