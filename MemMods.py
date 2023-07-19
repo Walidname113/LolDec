8e0faf28b4f3928c98189cde1c3f2c74328b6330
Walidname113 <100084394+Walidname113@users.noreply.github.com>
2023-07-19 21:54:49 +0300

    Add files via upload

---
+++ MemMods.py
@@ -0,0 +1,56 @@
+# meta developer: @LolDecMods
+# scope: MemMods
+# scope: MemMods 0.0.1
+from .. import loader
+
+
+@loader.tds
+class MemMods(loader.Module):
+    """Много мемов!\nСледите за обновлениями,со временем их будет больше."""
+
+    strings = {"name": "MemMods"}
+
+    async def hihicmd(self, message):
+        """Отправляет гс с громким хихихиха"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/filechannelscp/43",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+        
+    async def niggacmd(self, message):
+        """Отправляет мемное видео"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/filechannelscp/42",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+        
+        
+        
+    async def bagvideocmd(self, message):
+        """Отправляет багованное видео"""
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            "https://t.me/filechannelscp/44",
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
+        return
+        
+        
+   
+        
